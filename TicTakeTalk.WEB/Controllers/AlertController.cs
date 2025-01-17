dd2c38903c62f38a3e6fa7b6f0f606d44e9afc43
kyeongjin.park <kyeongjin.park@gowit.co.kr>
2025-01-17 17:59:55 +0900

    Feat: 화면 레이아웃 및 화면 구성 작업
    레이아웃, 로그인 레이아웃, 로그인 페이지, SubBar페이지 생성

---
+++ TicTakeTalk.WEB/Controllers/AlertController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicTakeTalk.Web.Controllers
+{
+    public class AlertController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AlertSubBar()
+        {
+            return View();
+        }
+    }
+}
---
+++ TicTakeTalk.WEB/Controllers/TalkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicTakeTalk.Web.Controllers
+{
+    public class TalkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TalkSubBar()
+        {
+            return View();
+        }
+    }
+}
